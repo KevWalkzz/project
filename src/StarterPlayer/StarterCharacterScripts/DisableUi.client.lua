e4b80876b65d5e722148157537aab495a551f0eb
KevWalkzz <Keven7568@gmail.com>
2025-05-24 12:22:57 -0300

    pcall for Disabling Core System UI

--- src/StarterPlayer/StarterCharacterScripts/DisableUi.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/DisableUi.client.lua
@@ -1,3 +1,11 @@
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-game.StarterGui:SetCore("ResetButtonCallback", false)
+local success, result = pcall(function(): ()
+	game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
+	game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+	game.StarterGui:SetCore("ResetButtonCallback", false)
+end)
+
+if success then
+	print("Ui disabled")
+else
+	warn("Failed to disable ui: ", result)
+end
